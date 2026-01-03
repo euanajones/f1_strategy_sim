479082c004dff1e21516d1164f7e9757bb4e111b
Euan Jones <euanajones@gmail.com>
2026-01-03 20:33:21 +0000

    Added strategy to driver class

--- src/Driver.hpp
+++ src/Driver.hpp
@@ -6,21 +6,26 @@
 #define F1_STRATEGY_SIM_DRIVER_HPP
 #include <string>
 
+#include "simulation.hpp"
+
 
 class Driver {
     public:
-        Driver(std::string driver_name, double base_pace_seconds, double consistency)
+        Driver(std::string driver_name, double base_pace_seconds, double consistency, const Strategy& strategy)
             :   driver_name_(std::move(driver_name)),
                 base_pace_seconds_(base_pace_seconds),
-                consistency_(consistency) {}
+                consistency_(consistency),
+                strategy_(std::move(strategy)) {}
 
         const std::string& driver_name() const {return driver_name_;};
         double base_pace() const {return base_pace_seconds_;}
         double consistency() const {return consistency_;}
+        const Strategy& strategy() const {return strategy_;}
     private:
         std::string driver_name_;
         double base_pace_seconds_;
         double consistency_;
+        Strategy strategy_;
 };
 
 
